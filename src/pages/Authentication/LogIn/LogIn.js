1f4f57656ad0eefeed0bb5f3d5eefd0bff3b1443
Ijaj Ahamed Nabil <37688324+ian-of-yore@users.noreply.github.com>
2022-11-18 16:59:09 +0600

    Register page design and validation done with react-form-hook

--- src/pages/Authentication/LogIn/LogIn.js
+++ src/pages/Authentication/LogIn/LogIn.js
@@ -10,9 +10,9 @@ const LogIn = () => {
     }
 
     return (
-        <div className='h-[480px] flex justify-center items-center'>
+        <div className='h-[480px] flex justify-center items-center mt-10'>
             <div className='w-96 p-8'>
-                <h2 className='text-2xl font-serif text-center mb-6'>Login</h2>
+                <h2 className='text-2xl font-mono font-semibold text-center mb-6'>Login</h2>
                 <form onSubmit={handleSubmit(handleLogin)}>
                     <div className="form-control w-full">
                         <label className="label"> <span className="label-text text-base pl-1">Email</span> </label>
@@ -27,10 +27,10 @@ const LogIn = () => {
                             {...register("password", { required: "Password is required!", minLength: { value: 6, message: "Password must be atleast 6 characters or more" } })}
                             className="input input-bordered w-80 h-11" />
                         {errors.password && <p className='text-red-600 text-sm font-semibold mt-1 text-center' role="alert">{errors.password?.message}</p>}
-                        <label className="label"> <span className="text-xs">Forgot Password?</span> </label>
+                        <label className="label"> <span className="text-xs underline">Forgot Password?</span> </label>
                     </div>
                     <input className='btn btn-accent w-full mt-3' value="Login" type="submit" />
-                    <p className='text-center mt-2'>New Here? <Link to='/register'><span className='text-sm text-secondary'>Create an account!</span></Link></p>
+                    <p className='text-center mt-2'>New Here? <Link to='/register'><span className='text-sm text-secondary underline'>Create an account!</span></Link></p>
                     <div className="divider">OR</div>
                     <button className='btn btn-outline w-full'>Continue With Google</button>
                 </form>
--- src/pages/Authentication/Register/Register.js
+++ src/pages/Authentication/Register/Register.js
@@ -1,9 +1,50 @@
 import React from 'react';
+import { useForm } from "react-hook-form";
+import { Link } from 'react-router-dom';
 
 const Register = () => {
+
+    const { register, formState: { errors }, handleSubmit } = useForm();
+
+    const handleRegister = (data) => {
+        console.log(data);
+    }
+
     return (
-        <div>
-            <h2>This is the register page</h2>
+        <div className='h-[480px] flex justify-center items-center mt-10'>
+            <div className='w-96 p-7 shadow-2xl'>
+                <h2 className='text-center text-2xl font-mono font-semibold my-3'>Register</h2>
+                <form onSubmit={handleSubmit(handleRegister)}>
+                    <div className="form-control">
+                        <label className="label"><span className="label-text">Name</span></label>
+                        <input {...register("name", { required: "We need to know your name!", maxLength: { value: 20, message: "Gotta keep your name within 20 characters!" } })}
+                            type="text" className="input input-bordered w-80 h-11" />
+                        {errors.name && <p className='text-red-600 text-center text-sm pt-2'>{errors.name?.message}</p>}
+                    </div>
+                    <div className="form-control">
+                        <label className="label"><span className="label-text">Email</span></label>
+                        <input {...register("email", { required: "Email is required to Register!" })}
+                            type="email" className="input input-bordered w-80 h-11" />
+                        {errors.email && <p className='text-red-600 text-center text-sm pt-2'>{errors.email?.message}</p>}
+                    </div>
+                    <div className="form-control">
+                        <label className="label"><span className="label-text">Password</span></label>
+                        <input {...register("password",
+                            {
+                                pattern: {
+                                    value: /^(?=(.*[a-z]))(?=(.*[A-Z]))(?=(.*[0-9]))(?=(.*[!@#$%^&*()\-__+.])).{8,}$/,
+                                    message: 'Create a storng password with special character, digit and uppercase letters worth of 8 characters in length'
+                                }
+                            })}
+                            type="password" className="input input-bordered w-80 h-11" />
+                        {errors.password && <p className='text-red-600 text-center text-xs pt-2'>{errors.password?.message}</p>}
+                    </div>
+                    <input className='btn btn-accent w-full mt-6' value="Register" type="submit" />
+                    <p className='text-center mt-2'>Already have an accout? <Link to='/login'><span className='text-secondary underline text-sm'>LogIn</span></Link></p>
+                </form>
+                <div className="divider">OR</div>
+                <button className='btn btn-outline w-full'>Continue With Google</button>
+            </div>
         </div>
     );
 };
